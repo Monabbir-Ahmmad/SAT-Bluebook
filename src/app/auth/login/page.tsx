b88468fcb6bfaf3cb806abdf22a73d2a9b624239
Monabbir-Ahmmad <monabbir.ahmmad@gmail.com>
2023-07-17 18:06:37 +0600

    update: login page style changes, scrollbar style changes

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,10 +1,25 @@
+import Link from "next/link";
 import LoginForm from "@/components/auth/LoginForm";
 import React from "react";
 
 export default function page() {
   return (
-    <div>
-      <LoginForm />
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-xl w-full mx-auto flex flex-col gap-3 p-4">
+        <h1 className="text-3xl font-bold text-center uppercase mb-5">
+          Login to continue
+        </h1>
+        <LoginForm />
+        <p>
+          Don&apos;t have an account?{" "}
+          <Link
+            className="daisy-link-primary font-semibold"
+            href="/auth/signup"
+          >
+            Register
+          </Link>
+        </p>
+      </div>
     </div>
   );
 }
