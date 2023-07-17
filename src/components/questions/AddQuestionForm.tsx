a7123e0cd4d246a27da696a1c698a79e26cf1f7b
anindomac <sarkeranindo@gmail.com>
2023-07-17 13:26:01 +0600

    new pages added

--- src/components/questions/AddQuestionForm.tsx
+++ src/components/questions/AddQuestionForm.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
 
 export default function AddQuestionForm() {
   return (
-    <div>AddQuestionForm</div>
-  )
+    <div>
+      <h1>Add Question Form</h1>
+      <ul className="flex items-center justify-center flex-col bg-gray-200">
+        <li>Question</li>
+        <li>Ans</li>
+        <li>Options</li>
+        <li>Tags</li>
+        <li>Subject</li>
+      </ul>
+    </div>
+  );
 }
