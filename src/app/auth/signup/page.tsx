aadb83bfe95ade4a00f7c71a2fc0544b1f1c2576
Monabbir-Ahmmad <monabbir.ahmmad@gmail.com>
2023-07-17 18:10:54 +0600

    update: auth page and form style changes with validation added

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,10 +1,22 @@
-import SignupForm from "@/components/auth/SignupForm";
+import Link from "next/link";
 import React from "react";
+import SignupForm from "@/components/auth/SignupForm";
 
 export default function page() {
   return (
-    <div>
-      <SignupForm />
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-xl w-full mx-auto flex flex-col gap-3 p-4">
+        <h1 className="text-3xl font-bold text-center uppercase mb-5">
+          Create an account
+        </h1>
+        <SignupForm />
+        <p>
+          Already have an account?{" "}
+          <Link className="daisy-link-primary font-semibold" href="/auth/login">
+            Login
+          </Link>
+        </p>
+      </div>
     </div>
   );
 }
