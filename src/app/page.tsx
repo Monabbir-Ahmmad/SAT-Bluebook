9f54544f9dd947c276e8f57f95a02c57a956094e
Monabbir Ahmmad Tajbir <monabbir.ahmmad@gmail.com>
2023-07-25 18:05:23 +0600

    Hero updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
+"use client";
+
 import Hero from "@/components/home/Hero";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       <Hero />
-      <Link href={"/dashboard"}>
-        <h2 className="text-2xl font-bold text-blue-300 underline md:text-3xl">
-          Go to dashboard
-        </h2>
-      </Link>
     </main>
   );
 }
