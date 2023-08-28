b298cab03a16257d6e8b3cca276b988146d394e5
Monabbir Ahmmad Tajbir <monabbir.ahmmad@gmail.com>
2023-08-28 11:13:45 +0600

    Slow css load fix

--- src/context/ThemeProvider.tsx
+++ src/context/ThemeProvider.tsx
@@ -4,10 +4,13 @@ import {
   ColorScheme,
   ColorSchemeProvider,
   MantineProvider,
+  useEmotionCache,
 } from "@mantine/core";
 
 import { AppFont } from "@/app/layout";
+import { CacheProvider } from "@emotion/react";
 import { Notifications } from "@mantine/notifications";
+import { useServerInsertedHTML } from "next/navigation";
 import { useState } from "react";
 
 interface ThemeProviderProps {
@@ -16,6 +19,17 @@ interface ThemeProviderProps {
 
 export default function ThemeProvider({ children }: ThemeProviderProps) {
   const [colorScheme, setColorScheme] = useState<ColorScheme>("light");
+  const cache = useEmotionCache();
+  cache.compat = true;
+
+  useServerInsertedHTML(() => (
+    <style
+      data-emotion={`${cache.key} ${Object.keys(cache.inserted).join(" ")}`}
+      dangerouslySetInnerHTML={{
+        __html: Object.values(cache.inserted).join(" "),
+      }}
+    />
+  ));
 
   const toggleColorScheme = (value?: ColorScheme) => {
     const nextColorScheme =
@@ -24,18 +38,20 @@ export default function ThemeProvider({ children }: ThemeProviderProps) {
   };
 
   return (
-    <ColorSchemeProvider
-      colorScheme={colorScheme}
-      toggleColorScheme={toggleColorScheme}
-    >
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{ fontFamily: AppFont.style.fontFamily }}
+    <CacheProvider value={cache}>
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
       >
-        <Notifications position="bottom-center" />
-        {children}
-      </MantineProvider>
-    </ColorSchemeProvider>
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{ fontFamily: AppFont.style.fontFamily }}
+        >
+          <Notifications position="bottom-center" />
+          {children}
+        </MantineProvider>
+      </ColorSchemeProvider>
+    </CacheProvider>
   );
 }
