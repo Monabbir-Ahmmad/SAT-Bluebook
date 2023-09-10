bab55c8bd88a73b727e7b5d7d382d06c2a86f0bc
Monabbir-Ahmmad <monabbir.ahmmad@gmail.com>
2023-09-10 21:27:43 +0600

    db connection moved to async handler

--- src/lib/server/actions/index.ts
+++ src/lib/server/actions/index.ts
@@ -3,9 +3,6 @@ import ExamAction from "./exam.action";
 import QuestionAction from "./question.action";
 import QuestionSetAction from "./question-set.action";
 import UserAction from "./user.action";
-import connectDB from "../config/connect-db";
-
-connectDB();
 
 export const authAction = new AuthAction();
 export const userAction = new UserAction();
