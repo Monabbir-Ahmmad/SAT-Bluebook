477e175b024619ba674a452a4d9e4065f4c438bc
Monabbir-Ahmmad <monabbir.ahmmad@gmail.com>
2023-09-22 04:44:11 +0600

    admin access fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,22 @@
 import { NextRequestWithAuth, withAuth } from "next-auth/middleware";
 
+import { NextResponse } from "next/server";
 import { UserRoles } from "./constants/enums";
 
 function authMiddleware(req: NextRequestWithAuth) {
   //Role based authorization
-  if (
-    req.nextUrl.pathname.includes("/student") &&
-    req.nextauth.token?.role !== UserRoles.USER
-  ) {
-    //return NextResponse.rewrite(new URL("/unauthorized", req.nextUrl));
-  }
+  // if (
+  //   req.nextUrl.pathname.includes("/student") &&
+  //   req.nextauth.token?.role !== UserRoles.USER
+  // ) {
+  //   return NextResponse.rewrite(new URL("/unauthorized", req.nextUrl));
+  // }
 
   if (
     req.nextUrl.pathname.includes("/admin") &&
     req.nextauth.token?.role !== UserRoles.ADMIN
   ) {
-    // return NextResponse.rewrite(new URL("/unauthorized", req.nextUrl));
+    return NextResponse.rewrite(new URL("/unauthorized", req.nextUrl));
   }
 }
 
