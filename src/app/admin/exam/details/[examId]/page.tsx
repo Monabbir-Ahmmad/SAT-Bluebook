74bf3f5a315a8f7b8637d5acba19f583e7178d96
anindo <sarkeranindo@gmail.com>
2023-09-30 00:32:24 +0600

    csv table example

--- src/app/admin/exam/details/[examId]/page.tsx
+++ src/app/admin/exam/details/[examId]/page.tsx
@@ -1,170 +1,217 @@
 "use client";
 
-import { Button, Divider, Modal } from "@mantine/core";
-import {
-  MRT_ColumnDef,
-  MRT_RowSelectionState,
-  MantineReactTable,
-} from "mantine-react-table";
-import { examService, userManagementService } from "@/lib/client/services";
-import { useEffect, useState } from "react";
-import {
-  useIsMutating,
-  useMutation,
-  useQuery,
-  useQueryClient,
-} from "@tanstack/react-query";
-
-import { RiCheckLine as CheckIcon } from "react-icons/ri";
-import DashboardCard from "@/components/dashboard/DashboardCard";
-import { ExamDto } from "@/dtos/exam.dto";
-import Link from "next/link";
-import { UserDto } from "@/dtos/user.dto";
-import { notifications } from "@mantine/notifications";
-
-const examTableColumns: MRT_ColumnDef<ExamDto>[] = [
+import { MantineReactTable, useMantineReactTable } from "mantine-react-table";
+import { Box, Button } from "@mantine/core";
+import { IconDownload } from "@tabler/icons-react";
+import { mkConfig, generateCsv, download } from "export-to-csv"; //or use your library of choice here
+const data = [
+  {
+    id: 1,
+    firstName: "Elenora",
+    lastName: "Wilkinson",
+    company: "Feest - Reilly",
+    city: "Hertaland",
+    country: "Qatar",
+  },
+  {
+    id: 2,
+    firstName: "Berneice",
+    lastName: "Feil",
+    company: "Deckow, Leuschke and Jaskolski",
+    city: "Millcreek",
+    country: "Nepal",
+  },
+  {
+    id: 3,
+    firstName: "Frieda",
+    lastName: "Baumbach",
+    company: "Heidenreich, Grady and Durgan",
+    city: "Volkmanside",
+    country: "Croatia",
+  },
+  {
+    id: 4,
+    firstName: "Zachery",
+    lastName: "Brown",
+    company: "Cormier - Skiles",
+    city: "Faychester",
+    country: "Saint Pierre and Miquelon",
+  },
+  {
+    id: 5,
+    firstName: "Kendra",
+    lastName: "Bins",
+    company: "Wehner - Wilderman",
+    city: "New Valentin",
+    country: "Senegal",
+  },
+  {
+    id: 6,
+    firstName: "Lysanne",
+    lastName: "Fisher",
+    company: "Schmidt LLC",
+    city: "Malachitown",
+    country: "Costa Rica",
+  },
+  {
+    id: 7,
+    firstName: "Garrick",
+    lastName: "Ryan",
+    company: "Ryan - Buckridge",
+    city: "East Pearl",
+    country: "Cocos (Keeling) Islands",
+  },
+  {
+    id: 8,
+    firstName: "Hollis",
+    lastName: "Medhurst",
+    company: "Quitzon Group",
+    city: "West Sienna",
+    country: "Papua New Guinea",
+  },
+  {
+    id: 9,
+    firstName: "Arlo",
+    lastName: "Buckridge",
+    company: "Konopelski - Spinka",
+    city: "Chino",
+    country: "Congo",
+  },
+  {
+    id: 10,
+    firstName: "Rickie",
+    lastName: "Auer",
+    company: "Lehner - Walsh",
+    city: "Nyahfield",
+    country: "Sudan",
+  },
+  {
+    id: 11,
+    firstName: "Isidro",
+    lastName: "Larson",
+    company: "Reichert - Paucek",
+    city: "Fort Rosinaside",
+    country: "Belize",
+  },
+  {
+    id: 12,
+    firstName: "Bettie",
+    lastName: "Skiles",
+    company: "Zulauf, Flatley and Rolfson",
+    city: "West Feltonchester",
+    country: "Poland",
+  },
+];
+//defining columns outside of the component is fine, is stable
+const columns = [
   {
     accessorKey: "id",
-    header: "#",
+    header: "ID",
+    size: 40,
   },
   {
-    accessorKey: "title",
-    header: "Title",
+    accessorKey: "firstName",
+    header: "First Name",
+    size: 120,
   },
   {
-    accessorKey: "createdAt",
-    header: "Created At",
-    Cell: ({ row }) => new Date(row.original.createdAt).toLocaleString(),
+    accessorKey: "lastName",
+    header: "Last Name",
+    size: 120,
   },
   {
-    accessorKey: "assignedTo",
-    header: "Assigned To",
-    Cell: ({ row }) => row.original.assignedTo.length,
+    accessorKey: "company",
+    header: "Company",
+    size: 300,
   },
   {
-    accessorKey: "attendedBy",
-    header: "Attended By",
-    Cell: ({ row }) => row.original.attendedBy.length,
+    accessorKey: "city",
+    header: "City",
+  },
+  {
+    accessorKey: "country",
+    header: "Country",
+    size: 220,
   },
 ];
 
-const userTableColumns: MRT_ColumnDef<UserDto>[] = [
-  {
-    accessorKey: "id",
-    header: "#",
-  },
-  { accessorKey: "name", header: "Name" },
-  { accessorKey: "email", header: "Email" },
-];
+const csvConfig = mkConfig({
+  fieldSeparator: ",",
+  decimalSeparator: ".",
+  useKeysAsHeaders: true,
+});
 
 export default function AdminExamDetailsPage() {
-  const queryClient = useQueryClient();
-  const isMutating = useIsMutating({ mutationKey: ["assign-exam"] });
+  const handleExportRows = (rows) => {
+    const rowData = rows.map((row) => row.original);
+    const csv = generateCsv(csvConfig)(rowData);
+    download(csvConfig)(csv);
+  };
 
-  const [examToAssign, setExamToAssign] = useState<ExamDto>();
-  const [selectedUsers, setSelectedUsers] = useState<MRT_RowSelectionState>({});
+  const handleExportData = () => {
+    const csv = generateCsv(csvConfig)(data);
+    download(csvConfig)(csv);
+  };
 
-  const { data: exams = [], isFetching: isExamsLoading } = useQuery({
-    queryKey: ["exams"],
-    queryFn: examService.getList,
+  const table = useMantineReactTable({
+    columns,
+    data,
+    enableRowSelection: true,
+    columnFilterDisplayMode: "popover",
+    paginationDisplayMode: "pages",
+    positionToolbarAlertBanner: "bottom",
+    renderTopToolbarCustomActions: ({ table }) => (
+      <Box
+        sx={{
+          display: "flex",
+          gap: "16px",
+          padding: "8px",
+          flexWrap: "wrap",
+        }}
+      >
+        <Button
+          color="lightblue"
+          //export all data that is currently in the table (ignore pagination, sorting, filtering, etc.)
+          onClick={handleExportData}
+          leftIcon={<IconDownload />}
+          variant="filled"
+        >
+          Export All Data
+        </Button>
+        <Button
+          disabled={table.getPrePaginationRowModel().rows.length === 0}
+          //export all rows, including from the next page, (still respects filtering and sorting)
+          onClick={() =>
+            handleExportRows(table.getPrePaginationRowModel().rows)
+          }
+          leftIcon={<IconDownload />}
+          variant="filled"
+        >
+          Export All Rows
+        </Button>
+        <Button
+          disabled={table.getRowModel().rows.length === 0}
+          //export all rows as seen on the screen (respects pagination, sorting, filtering, etc.)
+          onClick={() => handleExportRows(table.getRowModel().rows)}
+          leftIcon={<IconDownload />}
+          variant="filled"
+        >
+          Export Page Rows
+        </Button>
+        <Button
+          disabled={
+            !table.getIsSomeRowsSelected() && !table.getIsAllRowsSelected()
+          }
+          //only export selected rows
+          onClick={() => handleExportRows(table.getSelectedRowModel().rows)}
+          leftIcon={<IconDownload />}
+          variant="filled"
+        >
+          Export Selected Rows
+        </Button>
+      </Box>
+    ),
   });
 
-  const { data: users = [], isFetching: isUsersLoading } = useQuery({
-    queryKey: ["users"],
-    queryFn: userManagementService.getUsers,
-  });
-
-  const { mutate: assignExamMutation } = useMutation({
-    mutationKey: ["assign-exam"],
-    mutationFn: examService.assignExam,
-    onMutate: () => {
-      notifications.show({
-        id: "assign-exam",
-        title: "Assigning Exam",
-        message: "Please wait while we assign the exam to the students.",
-        loading: true,
-        autoClose: false,
-        withCloseButton: false,
-      });
-    },
-    onSuccess: (data) => {
-      notifications.update({
-        id: "assign-exam",
-        title: "Exam Assigned",
-        message: "Exam has been assigned to the students.",
-        color: "green",
-        icon: <CheckIcon />,
-      });
-
-      setExamToAssign(undefined);
-      queryClient.setQueryData<ExamDto[]>(
-        ["exams"],
-        (prev) => prev?.map((exam) => (exam.id === data.id ? data : exam)) || []
-      );
-    },
-    onError: (err: any) => {
-      notifications.update({
-        id: "assign-exam",
-        title: "Error",
-        message: err.response?.data?.message,
-        color: "red",
-      });
-    },
-  });
-
-  const onAssignToStudentClick = (data: ExamDto) => setExamToAssign(data);
-
-  const onExamAssign = () =>
-    assignExamMutation({
-      examId: examToAssign?.id!,
-      userIds: Object.keys(selectedUsers),
-    });
-
-  useEffect(() => {
-    setSelectedUsers(
-      examToAssign?.assignedTo.reduce(
-        (acc, user) => ({ ...acc, [user.id]: true }),
-        {}
-      ) || {}
-    );
-  }, [examToAssign]);
-
-  return (
-    <div className="min-h-full p-6 space-y-4">
-      <Divider
-        label={<h1 className="text-text-color font-semibold">Exam details</h1>}
-      />
-
-      <DashboardCard
-        label={"Create New Exam"}
-        description={"Create a new exam to assign to students"}
-        image="https://img.icons8.com/clouds/256/000000/document.png"
-      />
-
-      <Divider
-        label={<h1 className="text-text-color font-semibold">Exams</h1>}
-      />
-
-      <MantineReactTable
-        columns={examTableColumns}
-        data={exams}
-        state={{ isLoading: isExamsLoading }}
-        enableRowActions
-        positionActionsColumn="last"
-        renderRowActions={({ row }) => (
-          <div className="flex gap-4 items-center">
-            <Button onClick={() => onAssignToStudentClick(row.original)}>
-              Assign To Student
-            </Button>
-            <Link
-              href={`/admin/exam/details/${row.original.id}`}
-              className="text-blue-500 hover:text-blue-600 underline"
-            >
-              Details
-            </Link>
-          </div>
-        )}
-      />
-    </div>
-  );
+  return <MantineReactTable table={table} />;
 }
